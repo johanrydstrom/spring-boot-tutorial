a07f4047c791eae64feae84b0b445f9e1fd6f8ba
Johan Rydström <johan.rydstrom@postkodlotteriet.se>
2015-01-15 21:28:09 +0100

    Hystrix monitoring.

--- src/main/java/boot/service/TVService.java
+++ src/main/java/boot/service/TVService.java
@@ -2,11 +2,13 @@ package boot.service;
 
 import boot.dto.ScheduleDTO;
 import boot.exception.NotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.util.concurrent.ListenableFuture;
 import org.springframework.web.client.AsyncRestTemplate;
 import org.springframework.web.context.request.async.DeferredResult;
+import rx.Observable;
 
 import java.util.Arrays;
 import java.util.List;
@@ -14,13 +16,15 @@ import java.util.List;
 @Service
 public class TVService {
 
+    @Autowired private TVIntegration tvIntegration;
+
     private AsyncRestTemplate restTemplate = new AsyncRestTemplate();
 
     public DeferredResult<ScheduleDTO> nextOnAir(String showTitle) {
 
         DeferredResult<ScheduleDTO> deferredResult = new DeferredResult<>();
 
-        DeferredResult<List<ScheduleDTO>> scheduledPrograms = getScheduledPrograms("svt1");
+        DeferredResult<List<ScheduleDTO>> scheduledPrograms = getScheduledProgramsHystrix("svt1");
 
         scheduledPrograms.setResultHandler(svt1Programs -> {
             ((List<ScheduleDTO>) svt1Programs).stream()
@@ -47,4 +51,17 @@ public class TVService {
         return deferredResult;
     }
 
+    public DeferredResult<List<ScheduleDTO>> getScheduledProgramsHystrix(String channel) {
+        // Create a result to return immediately, and to set callbacks on
+        final DeferredResult<List<ScheduleDTO>> deferredResult = new DeferredResult<>();
+
+        // Fetch an observable result of our Hystrix request
+        Observable<List<ScheduleDTO>> observable = tvIntegration.getSchedule(channel);
+
+        // subscribe to the observable and invoke the methods on next
+        observable.subscribe(deferredResult::setResult,
+                deferredResult::setErrorResult);
+
+        return deferredResult;
+    }
 }
